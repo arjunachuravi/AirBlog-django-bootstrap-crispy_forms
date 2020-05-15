d39a552d7bbb8f8b9cd1cbc846fab15e4fb09948
arjunachuravi <arjunachuravi@gmail.com>
2020-05-15 13:53:45 +0530

    added list view

--- blogpost/urls.py
+++ blogpost/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import GreetingView
+from .views import GreetingView,postlistView
 
 app_name = "blogpost"
 urlpatterns = [
 
     path('greet/', GreetingView.as_view(greeting="blogpost greet Good Day")),
+    path('list/', postlistView.as_view()),
     
 ]
--- blogpost/views.py
+++ blogpost/views.py
@@ -2,8 +2,15 @@ from django.shortcuts import render
 from .models import myprojectblog
 from django.views import View
 
+from django.views.generic.list import ListView
+
 class GreetingView(View):
     greeting = ""
 
     def get(self, request):
         return render(request, 'greet.html', {"greeting":self.greeting})
+
+class postlistView(View):
+
+    def get(self,request):
+        return render(request,"postlist.html",{"objects":myprojectblog.objects.all()})
